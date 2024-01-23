3a090759eddb62900a16fb94b09c143d79465527
Netherest <contact@angusforrest.com>
2024-01-23 14:44:37 +1300

    Added Potential class to __init__

--- src/lbparticles/__init__.py
+++ src/lbparticles/__init__.py
@@ -1,6 +1,6 @@
 __all__ = ["Precomputer", "Particle", "PerturbationWrapper", "PotentialWrapper", "CartVec", "CylindVec",
            "LogPotential", "PowerlawPotential", "HernquistPotential", "NFWPotential", "VertOptionEnum"]
 
-from lbparticles.lbparticles import Precomputer, Particle, PerturbationWrapper, PotentialWrapper, CartVec, CylindVec, VertOptionEnum
+from lbparticles.lbparticles import Precomputer, Particle, PerturbationWrapper, PotentialWrapper, CartVec, CylindVec, VertOptionEnum, Potential
 
 from lbparticles.potentials import LogPotential, PowerlawPotential, HernquistPotential, NFWPotential
