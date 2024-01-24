1d064936c323d268b42c74347741026d71454175
Netherest <contact@angusforrest.com>
2024-01-25 09:12:39 +1300

    Hernquist Potential Pytest

--- src/lbparticles/__init__.py
+++ src/lbparticles/__init__.py
@@ -1,6 +1,32 @@
-__all__ = ["Precomputer", "Particle", "PerturbationWrapper", "PotentialWrapper", "CartVec", "CylindVec",
-           "LogPotential", "PowerlawPotential", "HernquistPotential", "NFWPotential", "VertOptionEnum", "Potential"]
+__all__ = [
+    "Precomputer",
+    "Particle",
+    "PerturbationWrapper",
+    "PotentialWrapper",
+    "CartVec",
+    "CylindVec",
+    "LogPotential",
+    "PowerlawPotential",
+    "HernquistPotential",
+    "NFWPotential",
+    "VertOptionEnum",
+    "Potential",
+]
 
-from lbparticles.lbparticles import Precomputer, Particle, PerturbationWrapper, PotentialWrapper, CartVec, CylindVec, VertOptionEnum, Potential
+from lbparticles.lbparticles import (
+    Precomputer,
+    Particle,
+    PerturbationWrapper,
+    PotentialWrapper,
+    CartVec,
+    CylindVec,
+    VertOptionEnum,
+    Potential,
+)
 
-from lbparticles.potentials import LogPotential, PowerlawPotential, HernquistPotential, NFWPotential
+from lbparticles.potentials import (
+    LogPotential,
+    PowerlawPotential,
+    HernquistPotential,
+    NFWPotential,
+)
---
+++ tests/test_potentials.py
@@ -0,0 +1,33 @@
+from lbparticles.potentials import LogPotential, HernquistPotential
+import numpy as np
+import pytest
+
+
+@pytest.mark.parametrize("r", np.linspace(0.1, 400, 100))
+class Test_LogPotential:
+    def test_call(self, r):
+        psir = LogPotential(220.0)
+        psir(r)
+
+    def test_ddr(self, r):
+        psir = LogPotential(220.0)
+        psir.ddr(r)
+
+    def test_ddr2(self, r):
+        psir = LogPotential(220.0)
+        psir.ddr2(r)
+
+
+@pytest.mark.parametrize("r", np.linspace(0.1, 400, 100))
+class Test_HernquistPotential:
+    def test_call(self, r):
+        psir = HernquistPotential(2.2, vcirc=220.0)
+        psir(r)
+
+    def test_ddr(self, r):
+        psir = HernquistPotential(2.2, vcirc=220.0)
+        psir.ddr(r)
+
+    def test_ddr2(self, r):
+        psir = HernquistPotential(2.2, vcirc=220.0)
+        psir.ddr2(r)
